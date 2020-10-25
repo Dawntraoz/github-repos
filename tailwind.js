be17cd30d4cdb488d14f62f459c2499c9ce92ca1
albasilventefuentes <alba@square1.io>
2020-10-25 21:30:26 +0100

    feat: docs folder with the built created to use in github pages

--- tailwind.js
+++ tailwind.js
@@ -6,7 +6,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ["./src/**/*.vue"],
   theme: {
     extend: {
       screens: {
